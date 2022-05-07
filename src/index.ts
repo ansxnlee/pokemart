35316dcbb9a77b42a8167c1a9a5046d8cabccf8b
Anson Lee <anson.lee73@outlook.com>
2022-05-06 23:57:58 -0400

    created login resolvers and started on cookies

--- src/index.ts
+++ src/index.ts
@@ -2,36 +2,73 @@ import 'reflect-metadata'; // typegraphql dependency (must be before importing t
 import { MikroORM } from "@mikro-orm/core"
 import type { PostgreSqlDriver } from '@mikro-orm/postgresql'; // or any other driver package
 import { __prod__ } from "./constant"; // checks if we're in production
-//import { Item } from "./entities/Item";
 import mikroConfig from './mikro-orm.config';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './resolvers/hello';
 import { ItemResolver } from './resolvers/item';
+import { UserResolver } from './resolvers/user';
+import { MyContext } from './types';
 
 const main = async () => {
   // mikroORM setup
+  // access EntityManager via `em` property
   const orm = await MikroORM.init<PostgreSqlDriver>(mikroConfig);
-  console.log(orm.em); // access EntityManager via `em` property
 
-  // express and apollo setup
+  // express setup
   const app = express();
+
+  // CORS config
+  // const corsOption = {
+  //   origin: "https://studio.apollographql.com",
+  //   credentials: true
+  // }
+
+  // redis client setup 
+  const session = require("express-session");
+  let RedisStore = require("connect-redis")(session);
+  // redis@v4
+  const { createClient } = require("redis");
+  let redisClient = createClient({ legacyMode: true });
+  redisClient.connect().catch(console.error);
+
+  // redis cookie settings
+  app.use( 
+    session({
+      name: 'userCookieID',
+      store: new RedisStore({ 
+        client: redisClient,
+        disableTouch: true, // dont refresh timer on each session interaction
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24, // cookie lasts a day
+        sameSite: 'None', // csrf stuff
+        secure: !__prod__, // might cause bugs(?) if this is true in dev
+      },
+      saveUninitialized: false,
+      secret: "secret key change this later",
+      resave: false,
+    })
+  );
+
+  // apollo graphql setup
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver, ItemResolver],
+      resolvers: [HelloResolver, ItemResolver, UserResolver],
       validate: false
     }),
-    context: () => ({ em: orm.em.fork() })
+    context: ({ req, res }): MyContext => ({ em: orm.em.fork(), req, res }),
   });
   
   await apolloServer.start();
-  apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({ app, cors: {credentials: true, origin: true} });
 
-  app.listen(4000, () => {
-    console.log("express server started on localhost:4000")
+  const PORT = process.env.PORT || 4000
+
+  app.listen(PORT, () => {
+    console.log(`express server started on localhost:${PORT}`)
   })
-
 }
 
 main();
---
+++ src/resolvers/user.ts
@@ -0,0 +1,128 @@
+import { User } from "../entities/User";
+import { MyContext } from "../types";
+import { Arg, Mutation, Query, Resolver, Ctx, ObjectType, Field } from "type-graphql";
+import argon2 from 'argon2';
+
+@ObjectType()
+// potential error can be displayed on frontend
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  // response will either be a user or an error
+  @Field(() => [FieldError], {nullable: true})
+  errors?: FieldError[];
+
+  @Field(() => User, {nullable: true})
+  user?: User;
+}
+
+@Resolver()
+export class UserResolver {
+  // get all items
+  @Query(() => [User])
+  users(
+    @Ctx() { em }: MyContext
+  ): Promise<User []> {
+    return em.find(User, {});
+  }
+
+  // create a user
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg('username', () => String) username: string,
+    @Arg('password', () => String) password: string,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    await em.begin();
+    // server check for valid usernames
+    if (username.length < 3) {
+      return {
+        errors: [
+          {
+            field: 'username',
+            message: 'username is too short',
+          },
+        ],
+      }
+    }
+    // server check for valid passwords
+    if (password.length < 3) {
+      return {
+        errors: [
+          {
+            field: 'password',
+            message: 'password is too short',
+          },
+        ],
+      }
+    }
+    // plaintext password is never stored on database
+    const hashedPassword = await argon2.hash(password)
+    const user = new User(username, hashedPassword);
+    try {
+      em.persist(user);
+      await em.commit();
+    } catch(e) {
+      await em.rollback();
+      // this is a pretty hacky way to handle duplicate usernames
+      if(e.code === "23505") {
+        return {
+          errors: [
+            {
+              field: 'username',
+              message: 'username already exists',
+            },
+          ]
+        }
+      }
+      throw e;
+    }
+    return { user };
+  }
+
+  // user login
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg('username', () => String) username: string,
+    @Arg('password', () => String) password: string,
+    @Ctx() { em, req }: MyContext
+  ): Promise<UserResponse> {
+    await em.begin();
+    const user = await em.findOne(User, { username })
+    // returned error can be shown on frontend
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "user does not exist",
+          },
+        ],
+      };
+    }
+    const validPassword = await argon2.verify(user.password, password)
+    if (!validPassword) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "incorrect password",
+          },
+        ],
+      }
+    }
+
+    // @types/express-session: "1.17.x" removed the key string from SessionData interface
+    // they want us to explicitly declare expected types for this object
+    // manually adjusted node_module to include it again as a hacky way to resolve type error
+    req.session.userId = user.id; // stores userId so we can access later
+    
+    return { user };
+  }
+}
